6f1daa51ac53067c48fc082d6642057f7c22fe54
Samuel Drysén <samuel.drysen@live.se>
2024-08-03 04:14:31 +0200

    .

--- src/commands/music/join.js
+++ src/commands/music/join.js
@@ -1,4 +1,4 @@
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, VoiceConnectionStatus } = require('@discordjs/voice');
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -13,9 +13,11 @@ module.exports = {
   async execute(interaction) {
     const inputChannel = interaction.options.getChannel('channel');
 
+    let connection = undefined;
+
     if (!inputChannel) {
       const channel = await interaction.member.voice.channel;
-      joinVoiceChannel({
+      connection = joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
@@ -23,13 +25,33 @@ module.exports = {
       });
     }
     else {
-      joinVoiceChannel({
+      connection = joinVoiceChannel({
         channelId: inputChannel.id,
         guildId: inputChannel.guild.id,
         adapterCreator: inputChannel.guild.voiceAdapterCreator,
         selfDeaf: false
       });
     }
+
+    connection.on(VoiceConnectionStatus.Connecting, () => {
+      console.log('Connecting voice connection...');
+    })
+
+    connection.on(VoiceConnectionStatus.Destroyed, () => {
+      console.log('destroyed voice connection...');
+    })
+
+    connection.on(VoiceConnectionStatus.Disconnected, () => {
+      console.log('disconnected voice connection...');
+    });
+
+    connection.on(VoiceConnectionStatus.Ready, () => {
+      console.log('ready voice connection...')
+    })
+    
+    connection.on(VoiceConnectionStatus.Signalling, () => {
+      console.log('signaling voice connection...');
+    });
 
     await interaction.reply(`Ready to rock!`);
   }
--- src/events/voice-ready.js
+++
@@ -1,8 +0,0 @@
-const { VoiceConnectionStatus } = require('@discordjs/voice');
-
-module.exports = {
-  name: VoiceConnectionStatus.Ready,
-  execute(oldState, newState) {
-    console.log(`Voice connection ready!`);
-  }
-}
